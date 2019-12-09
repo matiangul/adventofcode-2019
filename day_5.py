cd19ce618764d51280debf7d2f4b5f2feab37f73
Mateusz Angulski <mateusz.angulski@assertis.co.uk>
2019-12-09 20:28:45 +0100

    Day 5 - Part 2

--- day_5.py
+++ day_5.py
@@ -5,9 +5,13 @@ ADD = 1
 MULTIPLY = 2
 SAVE_INPUT = 3
 OUTPUT = 4
+JUMP_IF_TRUE = 5
+JUMP_IF_FALSE = 6
+LESS_THAN = 7
+EQUALS = 8
 
 
-def steps_to_move(opcode):
+def get_opcode_steps(opcode):
     if (opcode == ADD):
         return 4
     if (opcode == MULTIPLY):
@@ -16,7 +20,33 @@ def steps_to_move(opcode):
         return 2
     if (opcode == OUTPUT):
         return 2
+    if (opcode == JUMP_IF_TRUE):
+        return 3
+    if (opcode == JUMP_IF_FALSE):
+        return 3
+    if (opcode == LESS_THAN):
+        return 4
+    if (opcode == EQUALS):
+        return 4
     raise RuntimeError("Unknown opcode!", opcode)
+
+
+def get_next_iterator(iterator, integers, parameter_modes, opcode, parameters):
+    regular = iterator + get_opcode_steps(opcode)
+    if (opcode == JUMP_IF_TRUE):
+        to_compare = integers[parameters[0]
+                              ] if parameter_modes[0] == 0 else parameters[0]
+        new_pointer = integers[parameters[1]
+                               ] if parameter_modes[1] == 0 else parameters[1]
+        return new_pointer if to_compare != 0 else regular
+    elif (opcode == JUMP_IF_FALSE):
+        to_compare = integers[parameters[0]
+                              ] if parameter_modes[0] == 0 else parameters[0]
+        new_pointer = integers[parameters[1]
+                               ] if parameter_modes[1] == 0 else parameters[1]
+        return new_pointer if to_compare == 0 else regular
+    else:
+        return regular
 
 
 def run(integers, program_input, program_output, parameter_modes, opcode, parameters):
@@ -35,7 +65,25 @@ def run(integers, program_input, program_output, parameter_modes, opcode, parame
     elif (opcode == SAVE_INPUT):
         integers[parameters[0]] = int(program_input.pop(0))
     elif (opcode == OUTPUT):
-        program_output.append(integers[parameters[0]])
+        to_output = integers[parameters[0]
+                             ] if parameter_modes[0] == 0 else parameters[0]
+        program_output.append(to_output)
+    elif (opcode == LESS_THAN):
+        first = integers[parameters[0]
+                         ] if parameter_modes[0] == 0 else parameters[0]
+        second = integers[parameters[1]
+                          ] if parameter_modes[1] == 0 else parameters[1]
+        integers[parameters[2]] = 1 if first < second else 0
+    elif (opcode == EQUALS):
+        first = integers[parameters[0]
+                         ] if parameter_modes[0] == 0 else parameters[0]
+        second = integers[parameters[1]
+                          ] if parameter_modes[1] == 0 else parameters[1]
+        integers[parameters[2]] = 1 if first == second else 0
+    elif (opcode == JUMP_IF_TRUE):
+        return
+    elif (opcode == JUMP_IF_FALSE):
+        return
     else:
         raise RuntimeError("Unknown opcode!", opcode)
 
@@ -45,7 +93,7 @@ def intcode_computer(integers, program_input):
     iterator = 0
     while (integers[iterator] != HALT):
         opcode = integers[iterator] % 10
-        opcode_steps = steps_to_move(opcode)
+        opcode_steps = get_opcode_steps(opcode)
         parameter_count = opcode_steps - 1
         parameter_modes = [0] * parameter_count
         for m in range(2, parameter_count + 2):
@@ -53,11 +101,14 @@ def intcode_computer(integers, program_input):
         parameters = integers[iterator+1:iterator+opcode_steps]
         run(integers, program_input, program_output,
             parameter_modes, opcode, parameters)
-        iterator += opcode_steps
+        iterator = get_next_iterator(
+            iterator, integers, parameter_modes, opcode, parameters)
     return (integers, program_output)
 
 
 if __name__ == "__main__":
     intcode = list(map(int, stdin.readline().split(',')))
-    (_, program_output) = intcode_computer(intcode, [1])
-    print("Part 1: ", program_output.pop())
+    (_, program_output_1) = intcode_computer(intcode.copy(), [1])
+    print("Part 1: ", program_output_1.pop())
+    (_, program_output_2) = intcode_computer(intcode.copy(), [5])
+    print("Part 2: ", program_output_2.pop())
--- test_day_5.py
+++ test_day_5.py
@@ -1,21 +1,24 @@
 from sys import stdin
 import day_5
 
-def test_steps_to_move():
+
+def test_opcode_steps():
     try:
-        day_5.steps_to_move(day_5.HALT)
+        day_5.get_opcode_steps(day_5.HALT)
         assert True == False
     except:
         assert True == True
-    assert day_5.steps_to_move(day_5.ADD) == 4
-    assert day_5.steps_to_move(day_5.MULTIPLY) == 4
-    assert day_5.steps_to_move(day_5.SAVE_INPUT) == 2
-    assert day_5.steps_to_move(day_5.OUTPUT) == 2
+    assert day_5.get_opcode_steps(day_5.ADD) == 4
+    assert day_5.get_opcode_steps(day_5.MULTIPLY) == 4
+    assert day_5.get_opcode_steps(day_5.SAVE_INPUT) == 2
+    assert day_5.get_opcode_steps(day_5.OUTPUT) == 2
+
 
 def test_input_output():
     intcode = [3, 2, 0, 1, 4, 6, 99]
     (_, program_output) = day_5.intcode_computer(intcode, [4])
     assert program_output == [2, 99]
+
 
 def test_intcode_computer():
     intcode = [1101, 100, -1, 4, 0]
@@ -25,7 +28,27 @@ def test_intcode_computer():
     (resulting_intcode, _) = day_5.intcode_computer(intcode, [])
     assert resulting_intcode == [1002, 4, 3, 4, 99]
 
+
+def test_jumps_and_compares_intcode_computer():
+    intcode = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+               1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+               999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99]
+    (_, output) = day_5.intcode_computer(intcode, [8])
+    assert output.pop() == 1000
+    intcode_2 = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+                 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+                 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99]
+    (_, output_2) = day_5.intcode_computer(intcode_2, [7])
+    assert output_2.pop() == 999
+    intcode_3 = [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31,
+                 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104,
+                 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99]
+    (_, output_3) = day_5.intcode_computer(intcode_3, [9])
+    assert output_3.pop() == 1001
+
+
 my_input = list(map(int, stdin.readline().split(',')))
+
 
 def test_with_my_input():
     intcode = my_input.copy()
